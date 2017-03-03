516e5c5df65333489174ea6d70a4e42ec3d29a9b
rghossi <rafael.ghossi@gmail.com>
2017-03-03 16:40:54 -0300

    added accepting and declining trade requests test cases

--- test/trade_routes_spec.js
+++ test/trade_routes_spec.js
@@ -143,4 +143,154 @@ describe('routes : trades', () => {
       })
     })
   })
+
+  describe('PUT /api/trades/accept', () => {
+    it('should change trade status from "sent" to "accepted"', (done) => {
+      let book1 = new Book(myBook);
+      let book2 = new Book(hisBook);
+      book1.owner = myId;
+      book2.owner = hisId;
+      book1.save((err, myBook) => {
+        book2.save((err, hisBook) => {
+          let trade = new Trade({mine: myBook._id, theirs: hisBook._id})
+          trade.save((err, trade) => {
+            agent
+            .get('/api/logout')
+            .end((err, res) => {
+              agent
+              .post('/api/login')
+              .send({email: him.email, password: him.password})
+              .end((err, res) => {
+                agent
+                .put('/api/trades/accept')
+                .send({tradeId: trade._id})
+                .end((err, res) => {
+                  if (err) throw err;
+                  res.should.have.status(200);
+                  res.body.should.be.an('object');
+                  res.body.should.have.property('trade');
+                  const trade = res.body.trade;
+                  trade.should.have.property('_id');
+                  should.exist(trade.mine);
+                  should.exist(trade.theirs);
+                  trade.status.should.equal("accepted");
+                  Book.findById(myBook._id, (err, book) => {
+                    book.didTrade.should.equal(true);
+                    Book.findById(hisBook._id, (err, book2) => {
+                      book2.didTrade.should.equal(true);
+                      done();
+                    })
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+
+    it('should not change status if wrong user is loggedIn', (done) => {
+      let book1 = new Book(myBook);
+      let book2 = new Book(hisBook);
+      book1.owner = myId;
+      book2.owner = hisId;
+      book1.save((err, myBook) => {
+        book2.save((err, hisBook) => {
+          let trade = new Trade({mine: myBook._id, theirs: hisBook._id})
+          trade.save((err, trade) => {
+            agent
+            .put('/api/trades/accept')
+            .send({tradeId: trade._id})
+            .end((err, res) => {
+              res.should.have.status(403);
+              res.body.should.be.an('object');
+              res.body.should.not.have.property('trade');
+              Book.findById(myBook._id, (err, book) => {
+                if (book.didTrade) book.didTrade.should.equal(false);
+                Book.findById(hisBook._id, (err, book2) => {
+                  if (book2.didTrade) book2.didTrade.should.equal(false);
+                  done();
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+
+  describe('PUT /api/trades/decline', () => {
+    it('should change trade status from "sent" to "declined"', (done) => {
+      let book1 = new Book(myBook);
+      let book2 = new Book(hisBook);
+      book1.owner = myId;
+      book2.owner = hisId;
+      book1.save((err, myBook) => {
+        book2.save((err, hisBook) => {
+          let trade = new Trade({mine: myBook._id, theirs: hisBook._id})
+          trade.save((err, trade) => {
+            agent
+            .get('/api/logout')
+            .end((err, res) => {
+              agent
+              .post('/api/login')
+              .send({email: him.email, password: him.password})
+              .end((err, res) => {
+                agent
+                .put('/api/trades/decline')
+                .send({tradeId: trade._id})
+                .end((err, res) => {
+                  if (err) throw err;
+                  res.should.have.status(200);
+                  res.body.should.be.an('object');
+                  res.body.should.have.property('trade');
+                  const trade = res.body.trade;
+                  trade.should.have.property('_id');
+                  should.exist(trade.mine);
+                  should.exist(trade.theirs);
+                  trade.status.should.equal("declined");
+                  Book.findById(myBook._id, (err, book) => {
+                    if (book.didTrade) book.didTrade.should.equal(false);
+                    Book.findById(hisBook._id, (err, book2) => {
+                      if (book2.didTrade) book2.didTrade.should.equal(false);
+                      done();
+                    })
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+
+    it('should not change status if wrong user is loggedIn', (done) => {
+      let book1 = new Book(myBook);
+      let book2 = new Book(hisBook);
+      book1.owner = myId;
+      book2.owner = hisId;
+      book1.save((err, myBook) => {
+        book2.save((err, hisBook) => {
+          let trade = new Trade({mine: myBook._id, theirs: hisBook._id})
+          trade.save((err, trade) => {
+            agent
+            .put('/api/trades/decline')
+            .send({tradeId: trade._id})
+            .end((err, res) => {
+              res.should.have.status(403);
+              res.body.should.be.an('object');
+              res.body.should.not.have.property('trade');
+              Book.findById(myBook._id, (err, book) => {
+                if (book.didTrade) book.didTrade.should.equal(false);
+                Book.findById(hisBook._id, (err, book2) => {
+                  if (book2.didTrade) book2.didTrade.should.equal(false);
+                  done();
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+  })
 })
