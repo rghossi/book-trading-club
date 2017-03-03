d9461da5ecbf32445af0b4cf0f6e993e50b4e52f
rghossi <rafael.ghossi@gmail.com>
2017-03-03 11:40:42 -0300

    added new book route test cases

--- test/book_routes_spec.js
+++ test/book_routes_spec.js
@@ -16,9 +16,16 @@ describe('routes : books', () => {
     name: 'New Test User'
   };
 
+  const newBookData = {
+    name: "Dummy book",
+    author: "me",
+    isbn: "1269312879",
+    coverUrl: "http://hahahahahueheueh.com.br"
+  };
+
 	before((done) => {
 		User.createNew(newUserData, function(err, user){
-			if (err) throw err;
+			if (err.code !== 11000) throw err;
       done();
 		})
   });
@@ -33,7 +40,55 @@ describe('routes : books', () => {
 	beforeEach((done) => {
     Book.remove({}, function(err) { 
       if (err) throw err;
-      done();
+      agent
+	      .post('/api/login')
+	      .send({email: newUserData.email, password: newUserData.password})
+	      .end((err, res) => {
+	      	done();
+	      })
     });
   });
+
+  afterEach((done) => {
+  	agent
+	    .get('/api/logout')
+	    .end((err, res) => {
+	      if (err) throw err;
+	      done();
+	    })
+  })
+
+  describe('POST /api/books/new', () => {
+  	it ('should add new book and return it', (done) => {
+  		agent
+        .post('/api/books/new')
+        .send({book: newBookData})
+        .end((err, res) => {
+        	if (err) throw err;
+        		res.should.have.status(200);
+            res.body.should.be.an('object');
+            res.body.should.have.property('book');
+            const book = res.body.book;
+            book.should.have.property('_id');
+            book.name.should.equal(newBookData.name);
+            book.author.should.equal(newBookData.author);
+            book.isbn.should.equal(newBookData.isbn);
+            book.coverUrl.should.equal(newBookData.coverUrl);
+            should.exist(book.owner);
+            done();
+        })
+  	})
+
+  	it ('should not add new book if user is not logged in', (done) => {
+  		chai.request(server)
+        .post('/api/books/new')
+        .send({book: newBookData})
+        .end((err, res) => {
+      		res.should.have.status(401);
+          res.body.should.be.an('object');
+          should.not.exist(res.body.book);
+          done();
+        })
+  	})
+  })
 })
