1c57e790c439c7854ff21725ad582dc4d141087d
rghossi <rafael.ghossi@gmail.com>
2017-03-03 11:41:37 -0300

    added route and handler to add new book

---
+++ server/controllers/bookCtrl.js
@@ -0,0 +1,18 @@
+import User from '../models/user';
+import Book from '../models/book';
+
+export function addBook(req, res) {
+	if (!req.body.book) res.status(400).send({message: "no book"});
+	User.findById(req.user._id, function(err, user){
+		if (err) throw err;
+		if (!user) res.status(404).send({message: "User not found"});
+		else {
+			let newBook = new Book(req.body.book);
+			newBook.owner = user._id;
+			newBook.save((err, newBook) => {
+				if (err) throw err;
+				res.json({book: newBook});
+			})
+		}
+	})
+}
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import passport from 'passport';
 import * as UserController from './controllers/userCtrl';
+import * as BookController from './controllers/bookCtrl';
 
 const router = new Router();
 
@@ -12,6 +13,8 @@ router.post('/login', passport.authenticate('local'), function(req, res) {
     res.json({user: req.user});
   }
 );
+
+router.post("/books/new", UserController.loggedIn, BookController.addBook);
 
 
 export default router;
