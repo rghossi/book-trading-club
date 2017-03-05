8514fe3d8b607feffdbba23c6348b6c5ad798637
rghossi <rafael.ghossi@gmail.com>
2017-03-05 12:17:12 -0300

    remove unused commented code

--- client/index.jsx
+++ client/index.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Routes from './components/Routes';
-// import {AppContainer} from './components/app';
-// import {Provider} from 'react-redux';
-// import configureStore from './configureStore';
-// import {setState} from './actions';
 import './static/css/style.css';
 
-// const store = configureStore(socket);
-
-// render(<Provider store={store}>
-// 	<AppContainer />
-// </Provider>, document.getElementById('main'));
 render(<Routes />, document.getElementById('main'));
