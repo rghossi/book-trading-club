8c29d75757eb41149a5feef5796fbfb2ee78eb76
rghossi <rafael.ghossi@gmail.com>
2017-03-26 12:41:35 -0300

    added profile form

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,13 +1,78 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { Row, Col, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
 
 class Profile extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: "",
+			city: "",
+			state: ""
+		}
+	}
+
+	componentDidMount() {
+		const { user } = this.props;
+		if (user.name) this.setState({name: user.name})
+		if (user.city) this.setState({city: user.city})
+		if (user.state) this.setState({state: user.state})
+	}
+
+	handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleCityChange(e) {
+    this.setState({ city: e.target.value });
+  }
+
+  handleStateChange(e) {
+    this.setState({ state: e.target.value });
+  }
+
+  handleSubmit(e) {
+  	e.preventDefault();
+  	console.log(this.state);
+  }
+
   render() {
+  	let { user } = this.props;
     return (
-      <div>
-        Profile!
-      </div>
+      <Row>
+        <Col sm={6} smOffset={3} lg={4} lgOffset={4}> 
+          <form onSubmit={this.handleSubmit.bind(this)}>
+          	<FormGroup controlId="formBasicText">
+              <ControlLabel>Full Name</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.name}
+                onChange={this.handleNameChange.bind(this)}
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicText" >
+              <ControlLabel>City</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.city}
+                onChange={this.handleCityChange.bind(this)}
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicText">
+              <ControlLabel>State</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.state}
+                onChange={this.handleStateChange.bind(this)}
+              />
+            </FormGroup>
+            <Button type="submit">
+    		      Save
+    		    </Button>
+          </form>
+        </Col>
+      </Row>
     );
   }
 }
