bd5e76ba2b586459513832977665ed008165e36e
rghossi <rafael.ghossi@gmail.com>
2017-04-09 10:57:42 -0300

    fix signup bug that wasn't logging in user after signup

--- client/actions.js
+++ client/actions.js
@@ -65,7 +65,7 @@ export function signup(user) {
     		dispatch(errorSignup())
     	} else {
       	dispatch(successSignup(json))
-        dispatch(push('/'));
+        dispatch(login(user));
     	}
     })
   }
